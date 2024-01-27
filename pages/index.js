569490d60b49224c489a918cb3e742004a3405b1
Jhonatas Correa <99970414+CorreaJhow@users.noreply.github.com>
2024-01-27 01:58:20 +0000

    primeiro commit tentando realizar desafio do curso

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home() {
+    return <h1> Você é o amor da minha vida, e 
+        sei nunca vou me arrepender de ter voce e ser sobre voce 
+        todos os dias </h1>
+}
+
+export default Home; 
